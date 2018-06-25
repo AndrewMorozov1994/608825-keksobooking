48ba7d31cfada120f9071074e2e12bc631092ac7
AndrewMorozov1994 <35395445+AndrewMorozov1994@users.noreply.github.com>
2018-06-25 13:31:51 +0300

    Начат файл backend из модуля 6

---
+++ js/backend.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function () {
+  var URL_DOWNLOAD = 'https://js.dump.academy/keksobooking/data';
+  var URL_UPLOAD = 'https://js.dump.academy/keksobooking';
+  var timeout = 30000;
+
+  var setup = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Превышено время ожидания ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = timeout;
+
+    return xhr;
+  };
+})();
